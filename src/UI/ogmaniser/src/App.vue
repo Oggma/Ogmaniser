a26e2674d3cdb57a5a909ba368c4b39a2033a735
Raman Svezhantsau <raman.svezhantsau@gmail.com>
2021-11-11 19:54:58 +0300

    OGM-7 Developed main page

--- src/UI/ogmaniser/src/App.vue
+++ src/UI/ogmaniser/src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="header">
+    <img src="./assets/logo.png" />
   </div>
-  <router-view/>
+  <div id="main">
+    <div id="leftMenu">
+      <div id="nav">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </div>
+    </div>
+    <div id="content">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
@@ -12,18 +21,49 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #100f0f;
+  display: flex;
+  flex-direction: column;
+}
+
+#header {
+  background-color: #ff0c3e;
+  height: 60px;
+  display: flex;
+  padding: 0.3rem;
+}
+
+#leftMenu {
+  background-color: #b3abab;
+  width: 15%;
+}
+
+#main {
+  display: flex;
+  flex-direction: row;
+  height: calc(100vh - 80px);
+}
+
+#content {
+  padding: 1rem;
 }
 
 #nav {
-  padding: 30px;
+  display: flex;
+  flex-direction: column;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #100f0f;
+    text-decoration: none;
+    padding: 1rem;
+
+    &:hover {
+      background-color: #d7d7d7;
+    }
 
     &.router-link-exact-active {
-      color: #42b983;
+      background-color: #ff0c3d60;
     }
   }
 }
